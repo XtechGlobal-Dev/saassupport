feb729e53bc37814441811fe995c66cd0e5d86e6
ShivamXTec <shivam_mahajan@xtecglobal.com>
2025-08-06 16:30:53 +0530

    Resolved some issues

--- script/include/init.php
+++ script/include/init.php
@@ -126,13 +126,13 @@ function sb_component_chat()
                     </div>
                     <div class="sb-label-date-top"></div>
                 </div>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" style="margin-top: 163px;">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" style="margin-top: 160px; position: fixed; width: 385px; z-index: 8;">
                     <path fill="#155cfd" fill-opacity="1"
                         d="M0,96L60,133.3C120,171,240,245,360,266.7C480,288,600,256,720,224C840,192,960,160,1080,144C1200,128,1320,128,1380,128L1440,128L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z">
                     </path>
                 </svg>
 
-                <div class="sb-list sb-active"></div>
+                <div class="sb-list sb-active" style="margin-top: 233px;"></div>
                 <div class="sb-dashboard">
                     <div class="sb-dashboard-conversations">
                         <div class="sb-title">
