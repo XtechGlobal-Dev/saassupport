009a2afd1965b7391f0089fa83c0def00733dbcd
ShivamXTec <shivam_mahajan@xtecglobal.com>
2025-09-30 15:30:03 +0530

    chatboot issue solved

--- script/include/components.php
+++ script/include/components.php
@@ -149,7 +149,7 @@ function sb_profile_box()
                         </span>
                         <input type="password" id="edit-profile-password" />
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="toggleEditProfilePassword" width="24" height="24" color="#000000"
-                            fill="none" style="margin-left: -30px;margin-right: 10px; cursor: pointer;">
+                            fill="none" style="margin-left: -26px;margin-right: 10px; cursor: pointer;">
                             <path
                                 d="M21.544 11.045C21.848 11.4713 22 11.6845 22 12C22 12.3155 21.848 12.5287 21.544 12.955C20.1779 14.8706 16.6892 19 12 19C7.31078 19 3.8221 14.8706 2.45604 12.955C2.15201 12.5287 2 12.3155 2 12C2 11.6845 2.15201 11.4713 2.45604 11.045C3.8221 9.12944 7.31078 5 12 5C16.6892 5 20.1779 9.12944 21.544 11.045Z"
                                 stroke="#141B34" stroke-width="1.5" />
